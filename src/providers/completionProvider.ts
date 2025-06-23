9c42dcee0e55d14277a81aaf5f2cf1d4b74c3810
Lokesh Garg <lokeshgargadv@gmail.com>
2025-06-23 19:29:13 +0530

    Bug

---
+++ src/providers/completionProvider.ts
@@ -0,0 +1,64 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import isPositionInString from "../utils/isPositionInString";
+import isPositionInComment from "../utils/isPositionInComment";
+import extractClassNames from "../utils/extractClassNames";
+import getResolvedPath from "../utils/getPath";
+
+export default class CompletionItemProvider
+  implements vscode.CompletionItemProvider
+{
+  static config = vscode.workspace.getConfiguration("cssModulesIntellisense");
+  static aliasMap = CompletionItemProvider.config.get<Record<string, string>>(
+    "aliases",
+    {}
+  );
+
+  provideCompletionItems = async (
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ) => {
+    if (
+      (await isPositionInString(document, position)) ||
+      (await isPositionInComment(document, position))
+    ) {
+      return;
+    }
+
+    const line = document.lineAt(position).text;
+    const prefix = line.substring(0, position.character);
+    const match = prefix.match(/(\w+)\.([\w-]*)$/);
+    if (!match) {
+      return;
+    }
+
+    const [_, varName] = match;
+    const importRegex = new RegExp(
+      `import\\s+${varName}\\s+from\\s+['"](.+\\.module\\.(css|scss|less))['"]`
+    );
+    const fullText = document.getText();
+    const imp = fullText.match(importRegex);
+    if (!imp) {
+      return;
+    }
+
+    const resolvedPath = getResolvedPath(document, imp[1]);
+
+    if (!fs.existsSync(resolvedPath)) {
+      return;
+    }
+
+    const classNames = await extractClassNames(resolvedPath);
+    return new vscode.CompletionList(
+      classNames.map((name) => {
+        const item = new vscode.CompletionItem(
+          name,
+          vscode.CompletionItemKind.Variable
+        );
+        item.detail = "CSS Module class";
+        return item;
+      }),
+      false
+    );
+  };
+}
---
+++ src/providers/renameProvider.ts
@@ -0,0 +1,111 @@
+import * as vscode from "vscode";
+import isPositionInString from "../utils/isPositionInString";
+import isPositionInComment from "../utils/isPositionInComment";
+import getResolvedPath from "../utils/getPath";
+import getAllFiles from "../utils/getAllFiles";
+
+// TODO only works for the css module files, make a separate one for the script files
+export default class RenameProvider implements vscode.RenameProvider {
+  provideRenameEdits = async (
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    newName: string,
+    token: vscode.CancellationToken
+  ) => {
+    if (
+      (await isPositionInString(document, position)) ||
+      (await isPositionInComment(document, position))
+    ) {
+      return;
+    }
+
+    const wordRange = document.getWordRangeAtPosition(
+      position,
+      /\.[a-zA-Z0-9_-]+/
+    );
+    if (!wordRange) {
+      return;
+    }
+
+    const oldClassName = document.getText(wordRange).replace(/^\./, "");
+    const filePath = document.uri.fsPath;
+
+    const edit = new vscode.WorkspaceEdit();
+
+    const files = await getAllFiles();
+
+    // Update all the Javascript Files
+    await Promise.all(
+      files.map(async (file) => {
+        const doc = await vscode.workspace.openTextDocument(file);
+        const text = doc.getText();
+        const importRegex =
+          /import\s+(\w+)\s+from\s+['"](.*?\.module\.(css|scss|less))['"]/g;
+        let match: RegExpExecArray | null;
+
+        while ((match = importRegex.exec(text))) {
+          const varName = match[1];
+          const resolvedPath = getResolvedPath(doc, match[2]);
+
+          if (resolvedPath !== filePath) {
+            continue;
+          }
+
+          const usageRegex = new RegExp(`${varName}\\.${oldClassName}\\b`, "g");
+          let usageMatch: RegExpExecArray | null;
+
+          while ((usageMatch = usageRegex.exec(text))) {
+            const index =
+              usageMatch.index + usageMatch[0].indexOf(oldClassName);
+            const pos = doc.positionAt(index);
+
+            if (
+              (await isPositionInString(doc, pos)) ||
+              (await isPositionInComment(doc, pos))
+            ) {
+              continue;
+            }
+
+            const usageRange = new vscode.Range(
+              pos,
+              pos.translate(0, oldClassName.length)
+            );
+
+            edit.replace(doc.uri, usageRange, newName);
+          }
+        }
+      })
+    );
+
+    // Update the Css Module File
+    const text = document.getText();
+    const classNameRegex = new RegExp(`\\.${oldClassName}\\b`, "g");
+    let match: RegExpExecArray | null;
+
+    while ((match = classNameRegex.exec(text))) {
+      const offset = match.index;
+      const pos = document.positionAt(offset + 1);
+      const usageRange = new vscode.Range(
+        pos,
+        pos.translate(0, oldClassName.length)
+      );
+
+      edit.replace(document.uri, usageRange, newName);
+    }
+
+    return edit;
+  };
+
+  prepareRename = (
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ) => {
+    const wordRange = document.getWordRangeAtPosition(
+      position,
+      /\.[a-zA-Z0-9_-]+/
+    );
+    if (wordRange) {
+      return new vscode.Range(wordRange.start.translate(0, 1), wordRange.end);
+    }
+  };
+}
